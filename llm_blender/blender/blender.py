b918f7d341535bf12d82c245e13e6cdf514679f2
Yuu David Jinnai <ddyuudd@gmail.com>
2024-02-07 16:15:52 +0900

    Update blender.py

--- llm_blender/blender/blender.py
+++ llm_blender/blender/blender.py
@@ -221,6 +221,7 @@ class Blender:
                 batch = {k: v.to(self.ranker_config.device) for k, v in batch.items() if v is not None}
                 if self.ranker_config.ranker_type == "pairranker":
                     outputs = self.ranker._bubble_predict(**batch, num_runs=16)
+                    print('outputs=', outputs)
                     preds = outputs['logits'].detach().cpu().numpy()
                     batch_scores = get_scores_from_cmps(preds)
                 elif self.ranker_config.ranker_type in ["summareranker", "simcls"]:
